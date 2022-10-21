3d1c5c82ac9df609b9ffded80ae19397e9af2c44
Ahmet Karatas <ahmetkaratasdev@gmail.com>
2022-10-21 18:02:46 +1100

    added main.py file

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+   main.py
+   COMP9444, CSE, UNSW
+"""
+
+import torch
+import torch.nn as nn
+
+class neuralNetwork(torch.nn.Module):
+    def __init__(self, hid):
+
+      pass
+    
+    def forward(self, input):
+
+      pass
